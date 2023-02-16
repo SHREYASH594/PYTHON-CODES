cbf41e633224c480d3cd32c3004d6ccd1030bbd1
SHREYASH594 <121344075+SHREYASH594@users.noreply.github.com>
2023-02-17 00:12:20 +0530

    Add files via upload

---
+++ Remove all occurrences of a character from a string.py
@@ -0,0 +1,12 @@
+
+print("Remove all occurrences of a character from a string\n")
+str = input("Enter the string: ")
+char = input("Enter the character you want to remove: ")
+str1 = ""
+for i in range(len(str)):
+    if char == str[i]: 
+        continue
+    else:
+        str1 += str[i]
+print()
+print(str1)
---
+++ Remove the first occurrence of a character from a string.py
@@ -0,0 +1,12 @@
+print("Remove the first occurrence of a character from a string\n")
+str = input("Enter the string: ")
+char = input("Enter the character you want to remove: ")
+str1 = ""
+n = 0
+for i in range(len(str)):
+    if char == str[i] and n==0: 
+        n += 1
+    else:
+        str1 += str[i]
+print()
+print(str1)
